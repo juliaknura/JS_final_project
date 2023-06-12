2a6d4e4be246becbd23eecbc2a35dae9b6afaab6
Julia Knura <julia.knura25@gmail.com>
2023-06-12 10:40:46 +0200

    function names commit

--- program/Tasker.py
+++ program/Tasker.py
@@ -1,1 +1,32 @@
 from program.select import by_ddl, by_category, by_checked_off_date, by_exec_date
+from sqlalchemy import create_engine
+from datetime import datetime
+
+# TODO uzupelnic nazwe databazy - i tak bedzie jedna nasza wiec wystarczy jako zmienna wyszczegolniona w programie
+db_name = ...
+
+
+class Tasker:
+
+    def __init__(self):
+        self.current_task_list = self.get_today_list()
+        self.engine = create_engine(db_name, echo=True)
+        # na razie niech bedzie verbose zeby ogladac co sie dzieje, potem to zmienimy
+
+    def get_by_category(self, cat_id):
+        task_tuple_list = by_category(cat_id, self.engine)
+        # TODO - find out the format of the output of the selecting function, then proceed
+        task_list = []
+        return task_list
+
+    def get_today_list(self):
+        task_tuple_list = ...
+        task_list = []
+        return task_list
+
+    def get_by_checked_off_date(self, checked_off_date: datetime):
+        task_list = []
+        return task_list
+
+    def load_settings(self):
+        ...
--- program/select.py
+++ program/select.py
@@ -1,25 +1,25 @@
 from datetime import datetime
 
 
-def get_category_id(cat_name):
+def get_category_id(cat_name, engine):
     ...
 
 
-def by_category(cat_id):
+def by_category(cat_id, engine):
     ...
 
 
-def by_ddl(deadline: datetime):
+def by_ddl(deadline: datetime, engine):
     ...
 
 
-def by_exec_date(execution_date: datetime):
+def by_exec_date(execution_date: datetime, engine):
     ...
 
 
-def unchecked_tasks():
+def unchecked_tasks(engine):
     ...
 
 
-def by_checked_off_date(checked_off_date: datetime):
+def by_checked_off_date(checked_off_date: datetime, engine):
     ...
