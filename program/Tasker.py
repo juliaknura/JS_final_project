846ecfcdfc78d5263f3e981fffd87064a9af22d3
J.Stoch <j.stochniel@gmail.com>
2023-06-14 20:07:57 +0200

    Finished moving settings to new file and cleaning Tasker class from them

--- program/Tasker.py
+++ program/Tasker.py
@@ -4,8 +4,9 @@ from program import db_tables
 from program.selecter import by_ddl, by_category, by_checked_off_date, by_exec_date
 from program.selecter import by_ddl, by_category, by_checked_off_date, by_exec_date, \
     get_category_name, get_category_list, get_category_id, unchecked_tasks
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, update, delete
 from datetime import datetime, timedelta
+from program.db_tables import Tasks
 from program.db_tables import db_name
 from program.Task import Task
 
@@ -13,19 +14,17 @@ from program.Task import Task
 class Tasker:
     """This class handles adding, loading and editing tasks"""
 
-    def __init__(self):  # TODO powinien dostać priority_dict i daily_list_priority_lvl
+    def __init__(self, priorities_dict, daily_list_prior_lvl):
         self.current_task_dict = None
         self.get_today_list()
         self.engine = create_engine(db_name, echo=True)
         # TODO na razie niech bedzie verbose zeby ogladac co sie dzieje, potem to zmienimy
-        p_dict, l_opt, d_list_p_lvl = self.load_settings()
-        self.priority_dict = p_dict
+        self.priority_dict = priorities_dict
         # A dictionary with default priority level settings:
         # level 0 - urgent
         # level 1 - coming
         # level 2 - far
-        self.language_option = l_opt
-        self.daily_list_priority_lvl = d_list_p_lvl
+        self.daily_list_priority_lvl = daily_list_prior_lvl
 
     def get_current_list(self):
         """Returns the current tasks in a sorted list"""
@@ -99,13 +98,13 @@ class Tasker:
             cat_list.append(cat_name)
         return cat_list
 
-    def change_priority_lvl_settings(self, urgent: int, coming: int, far: int):  # TODO ogarnąć
+    def change_priority_lvl_settings(self, urgent: int, coming: int, far: int):
         """Changes priority level settings (time windows assigned to each priority level) in the current app run"""
         self.priority_dict[0] = urgent
         self.priority_dict[1] = coming
         self.priority_dict[2] = far
 
-    def change_daily_list_priority_level_setting(self, new_lvl: int):  # TODO ogarnąć
+    def change_daily_list_priority_level_setting(self, new_lvl: int):
         """Changes daily list priority level setting in the current app run"""
         self.daily_list_priority_lvl = new_lvl
 
@@ -125,11 +124,33 @@ class Tasker:
 
     def add_task(self, task_id: int, name: str, cat: str, desc: str, exec_date: datetime, deadline: datetime,
                  is_checked: bool, checked_off_date: datetime):
-        """Add a new task to the database"""
+        """Adds a new task to the database"""
         task_priority = self._calculate_priority(deadline)
-
+        cat_id = get_category_id(cat, self.engine)
         with Session(self.engine) as session:
-            task = db_tables.Task(task_id, name, cat, desc, exec_date, deadline, is_checked,
-                                  checked_off_date, task_priority)
+            task = Tasks(task_id=task_id, name=name, cat=cat_id, desc=desc, exec_date=exec_date,
+                         deadline=deadline, is_checked=is_checked, checked_off_date=checked_off_date,
+                         task_priority=task_priority)
             session.add(task)
             session.commit()
+
+    def delete_task(self, task_id):
+        del self.current_task_dict[task_id]
+        query = (delete(Tasks)
+                 .where(task_id=task_id))
+
+        with self.engine.begin() as conn:
+            conn.execute(query)
+
+
+
+    def toggle_task(self, task_id):
+        """Changes the state of a chosen, stored task and updates database"""
+        task = self.current_task_dict[task_id]
+        task.toggle()
+        query = (update(Tasks)
+                 .where(Tasks.task_id == task_id)
+                 .values(is_checked=task.is_checked, checked_off_date=task.checked_off_date))
+
+        with self.engine.begin() as conn:
+            conn.execute(query)
